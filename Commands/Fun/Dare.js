9e305fb648ba409d25a28ba6450f41467dde8a6f
Neko Kun <123672103+NekoSenpai69@users.noreply.github.com>
2023-04-24 00:44:48 +0530

    Update Dare.js

--- Commands/Fun/Dare.js
+++ Commands/Fun/Dare.js
@@ -12,6 +12,6 @@ module.exports = {
       "https://dull-plum-panda-gear.cyclic.app/dare"
     );
     
-    await Miku.sendMessage(m.from, {image: { url: botImage4 },caption: `*${shibam.data}*`,}, { quoted: m });
+    await Miku.sendMessage(m.from, {image: { url: botImage4 },caption: `*${shibam.data.dare}*`,}, { quoted: m });
   },
 };
