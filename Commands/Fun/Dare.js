12a8e17d2967fc9be5ef5bf6828ccf1a06f8d1e3
Neko Kun <123672103+NekoSenpai69@users.noreply.github.com>
2023-05-13 00:57:59 +0530

    Update Dare.js

--- Commands/Fun/Dare.js
+++ Commands/Fun/Dare.js
@@ -12,6 +12,6 @@ module.exports = {
       "https://smiling-hosiery-bear.cyclic.app/weeb/dare"
     );
     
-    await Miku.sendMessage(m.from, {image: { url: botImage4 },caption: `*${shibam.data}*`,}, { quoted: m });
+    await Miku.sendMessage(m.from, {image: { url: botImage4 },caption: `*${shibam.data.dare}*`,}, { quoted: m });
   },
 };
